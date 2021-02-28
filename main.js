fecf98e793c3286e24a0cdb808ea498a454a2b99
eshaw28 <79688189+eshaw28@users.noreply.github.com>
2021-02-27 19:16:33 -0600

    Update main.js

--- main.js
+++ main.js
@@ -7,16 +7,16 @@ $(document).ready(function(){
 
  function getTableData(table) {
     const data = [],
-    state = [],
-    positive = [],
-    death = [];
+    Category = [],
+    2018 = [],
+    2019 = [];
     table.rows({ search: "applied" }).every(function() {
     const data = this.data();
-    state.push(data[0]);
-    positive.push(parseInt(data[3].replace(/\,/g, "")));
-    death.push(parseInt(data[5].replace(/\,/g, "")));
+    category.push(data[0]);
+    2018.push(parseInt(data[1].replace(/\,/g, "")));
+    2019.push(parseInt(data[2].replace(/\,/g, "")));
     });
-    data.push(state, positive, death);
+    data.push(category, 2018, 2019);
     return data;
 }
 
@@ -26,10 +26,7 @@ function createHighcharts(data){
     zoomType: 'xy'
     },
     title: {
-    text: "Coronavirus Outbreak in the US"
-    },
-    subtitle: {
-    text: "Update: November 6, 2020 from covidtracking.com <br>Click and drag in the plot area to zoom in"
+    text: "Different types of crime in Wichita, KS"
     },
     xAxis: [
     {
@@ -48,13 +45,13 @@ function createHighcharts(data){
     ],
     series: [
     {
-    name: "Confirmed Cases",
+    name: "2018",
     type: "lollipop",
     data: data[1],
     color: "orange"
     },
     {
-    name: "Deaths",
+    name: "2019",
     type: "lollipop",
     data: data[2],
     color: "red"
